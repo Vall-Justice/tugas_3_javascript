2144c87f5a62f6b06b9e971250d084fb64b6e31b
Vall-Justice <104562832+Vall-Justice@users.noreply.github.com>
2022-05-13 23:59:07 +0700

    Tugas 3 Javascipt

---
+++ tugas3.js
@@ -0,0 +1,17 @@
+// Tugas 3 niomic
+
+// FUNCTION TAMBAH LIMA
+
+/* function tambahLima(x) {
+  return x + 5;
+}
+*/
+
+const tambahLima = (x) => x + 5;
+let nilai = 8;
+
+// sebelum diolah di function
+console.log(nilai);
+
+console.log(tambahLima(nilai));
+
